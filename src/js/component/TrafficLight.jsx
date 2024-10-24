7df9405b32fff6540fff1e24431bc123a4e399bb
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-10-24 18:58:19 +0000

    2do - Ejercicio base terminado

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TrafficLightButton from "./TrafficLightButton";
 
 const TrafficLight = () => {
 
+    const [botonIluminado, setBotonIluminado] = useState(null);
+
+    const estadoColorBtn = (color) => {
+        if (botonIluminado === color){
+            setBotonIluminado(null);
+        }else{
+            setBotonIluminado(color);
+        }
+    };
+
     return(
-        <div className="traffic-light-box d-flex flex-column align-items-center">
-            <TrafficLightButton color ="rojo" className="mt-3"/>
-			<TrafficLightButton	color ="amarillo" className="mt-3"/>
-			<TrafficLightButton	color ="verde" className="mt-3"/>
+        <div className="traffic-light-container">
+            <div className="traffic-light-soporte"></div>
+            <div className="traffic-light-box d-flex flex-column align-items-center">
+                <TrafficLightButton color="rojo" iluminado={botonIluminado === "rojo"} onClick={() => estadoColorBtn("rojo")}
+                    className="mt-3" />;
+                <TrafficLightButton color="amarillo" iluminado={botonIluminado === "amarillo"} onClick={() => estadoColorBtn("amarillo")}
+                    className="mt-3" />;
+                <TrafficLightButton color="verde" iluminado={botonIluminado === "verde"} onClick={() => estadoColorBtn("verde")}
+                    className="mt-3" />;
+            </div>
         </div>
 
     );
--- src/js/component/TrafficLightButton.jsx
+++ src/js/component/TrafficLightButton.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 
-const TrafficLightButton = ({ color }) => {
-    const [iluminado, setIluminado] = useState(false);
-    const iluminaLuces = () => {
-        setIluminado(!iluminado);
-    }
+const TrafficLightButton = ({ color, iluminado, onClick }) => {
+    // const [iluminado, setIluminado] = useState(false);
+    // const iluminaLuces = () => {
+    //     setIluminado(!iluminado);
+    // }
   return (
     <button
-      className={`btn traffic-light-button ${color} ${iluminado ? "ilumina" : ""}` } onClick={iluminaLuces}></button>
+      className={`btn traffic-light-button ${color} ${iluminado ? "ilumina" : ""}` } onClick={onClick}></button>
   );
 }
 
